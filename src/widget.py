fe3201aff3b2b004d297c2270ab026105baa1667
ZET <andrey.voenkov2005@mail.ru>
2025-05-16 22:51:29 +1000

    Поменял код

--- src/widget.py
+++ src/widget.py
@@ -1,24 +1,17 @@
 from datetime import datetime
-from typing import List, Dict, Optional
+from typing import Optional
 
 
-def sort_by_date(data: ListDict, ascending: Optionalbool = False) -> ListDict:
+def sort_by_date(date_string: str, format_string: Optional[str] = '%d.%m.%Y') -> str:
     """
-    Сортирует список словарей по дате в формате ISO 8601
+    Форматирует дату из формата ISO 8601 в указанный формат
 
-    :param data: список словарей с полем 'date'
-    :param ascending: True - по возрастанию, False - по убыванию (по умолчанию)
-    :return: отсортированный список словарей
+    :param date_string: строка с датой в формате ISO 8601
+    :param format_string: формат вывода даты (по умолчанию '%d.%m.%Y')
+    :return: отформатированная дата
     """
-
-    def parse_date(item):
-        date_str = item.get('date')
-
-    if date_str:
-        try:
-            return datetime.fromisoformat(date_str)
+    try:
+        date_obj = datetime.fromisoformat(date_string)
+        return date_obj.strftime(format_string)
     except ValueError:
-    raise ValueError(f"Неверный формат даты: {date_str}")
-    return None
-
-    return sorted(data, key=parse_date, reverse=not ascending)
+        raise ValueError(f"Неверный формат даты: {date_string}")
